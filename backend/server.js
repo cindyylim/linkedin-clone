12ff5c54644774cb9a4b0404bb943f7345b69b71
Cindy <cindyl23@proton.me>
2025-05-07 11:33:25 -0700

    Add auth controller and routes

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import { connectDb } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    connectDb();
+})
