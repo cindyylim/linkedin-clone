15371b044e5e3d453fbfc50d491fd5bf37e8db55
Cindy <cindyl23@proton.me>
2025-05-09 21:09:07 -0700

    Add sidebar

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../lib/axios";
+import Sidebar from "../components/Sidebar";
+
+
 const HomePage = () => {
-    return <div></div>
-}
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+      <div className="hidden lg:block lg:col-span-1">
+        <Sidebar user={authUser} />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
