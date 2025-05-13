71377a142e8031428ea880a489ae602deb104a5e
Cindy <cindyl23@proton.me>
2025-05-12 20:37:40 -0700

    Add PostCreation file

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,25 @@
 import { useAuth } from "../hooks/useAuth";
 import Sidebar from "../components/Sidebar";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "../lib/axios";
+import PostCreation from "../components/PostCreation";
 
 const HomePage = () => {
   const { data: authUser } = useAuth();
+  const {data: posts} = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/posts");
+      return res.data;
+    },
+  });
   return (
     <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
       <div className="hidden lg:block lg:col-span-1">
         <Sidebar user={authUser} />
+      </div>
+      <div className="col-span-1 lg:col-span-2 order-first lg:order-none">
+          <PostCreation user={authUser}/>
       </div>
     </div>
   );
